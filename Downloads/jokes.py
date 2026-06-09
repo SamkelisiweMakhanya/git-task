00d8f408e2c87191de24203a08d81e38963d9dfe
Samkelisiwe Makhanya <samkelisiwemakhanya@yahoo.com>
2026-06-09 19:05:57 +0200

    Added a code from my local repository

---
+++ Downloads/jokes.py
@@ -0,0 +1,16 @@
+import random
+jokes = [
+    "Why don't scientists trust atoms? Because they make up everything!",
+    "What do you call fake spaghetti? An impasta!",
+    "Why did the scarecrow win an award? Because he was outstanding in his field!",
+    "Why don't skeletons fight each other? They don't have the guts!",
+    "What do you call cheese that isn't yours? Nacho cheese!",
+    "Why did the bicycle fall over? Because it was two-tired!",
+    "Why did the tomato turn red? Because it saw the salad dressing!",
+    "Why did the chicken join a band? Because it had the drumsticks!",
+    "Why did the developer go broke? Because he used up all his cache!"
+    "Why did the function return early? Because it had a lot of arguments!",
+    "Why did the chicken cross the road? To get to the other side!"
+]
+random_joke = random.choice(jokes)
+print(random_joke)
